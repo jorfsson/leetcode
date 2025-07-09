ed101d6cdf200853cb305c34f16f311660fab279
jorfsson <jeffreychea234@gmail.com>
2025-07-09 00:25:55 -0700

    added new commands and roadmap

--- leetcode/app.py
+++ leetcode/app.py
@@ -8,6 +8,194 @@ import regex
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
+neetcode_150 = {
+    "Arrays & Hashing": [
+        "Contains Duplicate",
+        "Valid Anagram",
+        "Two Sum",
+        "Group Anagrams",
+        "Top K Frequent Elements",
+        "Encode and Decode Strings",
+        "Product of Array Except Self",
+        "Valid Sudoku",
+        "Longest Consecutive Sequence"
+    ],
+    "Two Pointers": [
+        "Valid Palindrome",
+        "Two Sum II Input Array Is Sorted",
+        "3Sum",
+        "Container With Most Water",
+        "Trapping Rain Water"
+    ],
+    "Stack": [
+        "Valid Parentheses",
+        "Min Stack",
+        "Evaluate Reverse Polish Notation",
+        "Generate Parentheses",
+        "Daily Temperatures",
+        "Car Fleet",
+        "Largest Rectangle in Histogram"
+    ],
+    "Binary Search": [
+        "Binary Search",
+        "Search a 2D Matrix",
+        "Koko Eating Bananas",
+        "Find Minimum in Rotated Sorted Array",
+        "Search in Rotated Sorted Array",
+        "Time Based Key Value Store",
+        "Median of Two Sorted Arrays"
+    ],
+    "Sliding Window": [
+        "Best Time to Buy and Sell Stock",
+        "Longest Substring Without Repeating Characters",
+        "Longest Repeating Character Replacement",
+        "Permutation in String",
+        "Minimum Window Substring",
+        "Sliding Window Maximum"
+    ],
+    "Linked List": [
+        "Reverse Linked List",
+        "Merge Two Sorted Lists",
+        "Linked List Cycle",
+        "Reorder List",
+        "Remove Nth Node From End of List",
+        "Copy List with Random Pointer",
+        "Add Two Numbers",
+        "Find The Duplicate Number",
+        "LRU Cache",
+        "Merge K Sorted Lists",
+        "Reverse Nodes in K Group"
+    ],
+    "Trees": [
+        "Invert Binary Tree",
+        "Maximum Depth of Binary Tree",
+        "Diameter of Binary Tree",
+        "Balanced Binary Tree",
+        "Same Tree",
+        "Subtree of Another Tree",
+        "Lowest Common Ancestor of A Binary Search Tree",
+        "Binary Tree Level Order Traversal",
+        "Binary Tree Right Side View",
+        "Count Good Nodes in Binary Tree",
+        "Validate Binary Search Tree",
+        "Kth Smallest Element in a BST",
+        "Construct Binary Tree from Preorder and Inorder Traversal",
+        "Binary Tree Maximum Path Sum",
+        "Serialize and Deserialize Binary Tree"
+    ],
+    "Tries": [
+        "Implement Trie Prefix Tree",
+        "Design Add and Search Words Data Structure",
+        "Word Search II"
+    ],
+    "Heap / Priority Queue": [
+        "Kth Largest Element in a Stream",
+        "Last Stone Weight",
+        "K Closest Points to Origin",
+        "Kth Largest Element in an Array",
+        "Task Scheduler",
+        "Design Twitter",
+        "Find Median from Data Stream"
+    ],
+    "Intervals": [
+        "Insert Interval",
+        "Merge Intervals",
+        "Non Overlapping Intervals",
+        "Meeting Rooms",
+        "Meeting Rooms II",
+        "Minimum Interval to Include Each Query"
+    ],
+    "Greedy": [
+        "Maximum Subarray",
+        "Jump Game",
+        "Jump Game II",
+        "Gas Station",
+        "Hand of Straights",
+        "Merge Triplets to Form Target Triplet",
+        "Partition Labels",
+        "Valid Parenthesis String"
+    ],
+    "Backtracking": [
+        "Subsets",
+        "Combination Sum",
+        "Combination Sum II",
+        "Permutations",
+        "Subsets II",
+        "Word Search",
+        "Palindrome Partitioning",
+        "Letter Combinations of a Phone Number",
+        "N Queens"
+    ],
+    "Graphs": [
+        "Number of Islands",
+        "Max Area of Island",
+        "Clone Graph",
+        "Walls and Gates",
+        "Rotting Oranges",
+        "Pacific Atlantic Water Flow",
+        "Surrounded Regions",
+        "Course Schedule",
+        "Course Schedule II",
+        "Graph Valid Tree",
+        "Number of Connected Components in an Undirected Graph",
+        "Redundant Connection",
+        "Word Ladder"
+    ],
+    "Advanced Graphs": [
+        "Network Delay Time",
+        "Reconstruct Itinerary",
+        "Min Cost to Connect All Points",
+        "Swim in Rising Water",
+        "Alien Dictionary",
+        "Cheapest Flights within K Stops",
+    ],
+    "1D Dynamic Programming": [
+        "Climbing Stairs",
+        "Min Cost Climbing Stairs",
+        "House Robber",
+        "House Robber II",
+        "Longest Palindromic Substring",
+        "Palindromic Substrings",
+        "Decode Ways",
+        "Coin Change",
+        "Maximum Product Subarray",
+        "Word Break",
+        "Longest Increasing Subsequence",
+        "Partition Equal Subset Sum"
+    ],
+    "2D Dynamic Programming": [
+        "Unique Paths",
+        "Longest Common Subsequence",
+        "Best Time to Buy and Sell Stock with Cooldown",
+        "Coin Change II",
+        "Target Sum",
+        "Interleaving String",
+        "Longest Increasing Path in a Matrix",
+        "Distinct Subsequences",
+        "Edit Distance",
+        "Burst Balloons",
+        "Regular Expression Matching",
+    ],
+    "Bit Manipulation": [
+        "Single Number",
+        "Number of 1 Bits",
+        "Counting Bits",
+        "Reverse Bits",
+        "Missing Number",
+        "Sum of Two Integers",
+        "Reverse Integer"
+    ],
+    "Math & Geometry": [
+        "Rotate Image",
+        "Spiral Matrix",
+        "Set Matrix Zeroes",
+        "Happy Number",
+        "Plus One",
+        "Powx n",
+        "Multiply Strings",
+        "Detect Squares"
+    ]
+}
 
 
 PROBLEMS = ['two-sum', 'add-two-numbers', 'longest-substring-without-repeating-characters', 'median-of-two-sorted-arrays', 'longest-palindromic-substring', 'coin-change-ii', 'reverse-integer', 'count-good-nodes-in-binary-tree', 'regular-expression-matching', 'container-with-most-water', 'rotting-oranges', '3sum', 'letter-combinations-of-a-phone-number', 'remove-nth-node-from-end-of-list', 'valid-parentheses', 'merge-two-sorted-lists', 'generate-parentheses', 'merge-k-sorted-lists', 'reverse-nodes-in-k-group', 'diameter-of-binary-tree', 'search-in-rotated-sorted-array', 'valid-sudoku', 'combination-sum', 'combination-sum-ii', 'trapping-rain-water', 'multiply-strings', 'jump-game-ii', 'permutations', 'rotate-image', 'group-anagrams', 'powx-n', 'n-queens', 'maximum-subarray', 'spiral-matrix', 'jump-game', 'merge-intervals', 'insert-interval', 'permutation-in-string', 'subtree-of-another-tree', 'unique-paths', 'plus-one', 'climbing-stairs', 'edit-distance', 'set-matrix-zeroes', 'search-a-2d-matrix', 'minimum-window-substring', 'subsets', 'word-search', 'largest-rectangle-in-histogram', 'subsets-ii', 'decode-ways', 'detect-squares', 'interleaving-string', 'validate-binary-search-tree', 'same-tree', 'binary-tree-level-order-traversal', 'last-stone-weight', 'maximum-depth-of-binary-tree', 'construct-binary-tree-from-preorder-and-inorder-traversal', 'task-scheduler', 'balanced-binary-tree', 'distinct-subsequences', 'best-time-to-buy-and-sell-stock', 'binary-tree-maximum-path-sum', 'valid-palindrome', 'word-ladder', 'longest-consecutive-sequence', 'surrounded-regions', 'palindrome-partitioning', 'clone-graph', 'gas-station', 'palindromic-substrings', 'single-number', 'copy-list-with-random-pointer', 'word-break', 'linked-list-cycle', 'reorder-list', 'lru-cache', 'evaluate-reverse-polish-notation', 'maximum-product-subarray', 'find-minimum-in-rotated-sorted-array', 'min-stack', 'valid-parenthesis-string', 'two-sum-ii-input-array-is-sorted', 'min-cost-to-connect-all-points', 'redundant-connection', 'max-area-of-island', 'reverse-bits', 'number-of-1-bits', 'house-robber', 'binary-tree-right-side-view', 'number-of-islands', 'happy-number', 'reverse-linked-list', 'course-schedule', 'implement-trie-prefix-tree', 'course-schedule-ii', 'design-add-and-search-words-data-structure', 'word-search-ii', 'house-robber-ii', 'kth-largest-element-in-an-array', 'contains-duplicate', 'invert-binary-tree', 'daily-temperatures', 'longest-common-subsequence', 'kth-smallest-element-in-a-bst', 'network-delay-time', 'lowest-common-ancestor-of-a-binary-search-tree', 'min-cost-climbing-stairs', 'product-of-array-except-self', 'sliding-window-maximum', 'valid-anagram', 'meeting-rooms', 'meeting-rooms-ii', 'partition-labels', 'graph-valid-tree', 'missing-number', 'alien-dictionary', 'encode-and-decode-strings', 'kth-largest-element-in-a-stream', 'binary-search', 'swim-in-rising-water', 'walls-and-gates', 'find-the-duplicate-number', 'cheapest-flights-within-k-stops', 'find-median-from-data-stream', 'serialize-and-deserialize-binary-tree', 'longest-increasing-subsequence', 'best-time-to-buy-and-sell-stock-with-cooldown', 'burst-balloons', 'coin-change', 'number-of-connected-components-in-an-undirected-graph', 'longest-increasing-path-in-a-matrix', 'reconstruct-itinerary', 'counting-bits', 'top-k-frequent-elements', 'design-twitter', 'hand-of-straights', 'sum-of-two-integers', 'car-fleet', 'koko-eating-bananas', 'partition-equal-subset-sum', 'pacific-atlantic-water-flow', 'longest-repeating-character-replacement', 'non-overlapping-intervals', 'minimum-interval-to-include-each-query', 'merge-triplets-to-form-target-triplet', 'target-sum', 'k-closest-points-to-origin', 'time-based-key-value-store']
@@ -89,20 +277,20 @@ class LeetCode:
             return
         return self._problems[problem]['count']
     
-    def get_next_problem(self) -> str:
-        elements = [self._problems[e] for e in self._problems]
-        elements.sort(key=lambda x: x.get('revisit_date', datetime.now().date().isoformat()))
-        
-        for e in elements:
-            if 'revisit_date' in e and datetime.strptime(e['revisit_date'], "%Y-%m-%d").date() <= datetime.now().date():
-                print(e['url'])
-                return
-        for e in elements:
-            if 'revisit_date' not in e:
-                print(e['url'])
-                return
-        print("No problems available for revisiting today.")
+    def reset_count(self, problem: str):
+        self._problems[problem]['revisit_date'] = None
+        self._problems[problem]['count'] = 0
+        self._update_data()
     
+    def get_next_problem(self):
+        problems_to_revisit = {k: v for k, v in self._problems.items() if v['revisit_date'] and datetime.strptime(v['revisit_date'], "%Y-%m-%d").date() <= datetime.now().date()}
+        uncompleted_problems = {k: v for k, v in self._problems.items() if not v['revisit_date']}
+        
+        for k, v in problems_to_revisit.items():
+            yield k, v
+        for k, v in uncompleted_problems.items():
+            yield k, v
+
     def get_random_problem(self):
         elements = [self._problems[e] for e in self._problems]
         counts = np.array([[e["count"]] for e in elements])
@@ -128,13 +316,15 @@ class LeetCode:
 def create_new_json(jsonfile: TextIOWrapper):
     with open(data_uri/"problems.txt") as readfile:
         res = {}
-        for line in readfile:
-            name = line.split('/')[-1]
-            res[name] = {
-                'url': line.strip(),
-                'updated_datetime': datetime.now().isoformat(),
-                'count': 0
-            }
+        for topic in neetcode_150:
+            for problem in neetcode_150[topic]:
+                res[problem] = {
+                    'url': f"https://leetcode.com/problems/{'-'.join(problem.lower().split(' '))}",
+                    'updated_datetime': datetime.now().isoformat(),
+                    'revisit_date': None,
+                    'count': 0,
+                    'tag': topic
+                }
         jsonfile.seek(0)
         json.dump(res, jsonfile, indent=4)
         jsonfile.truncate()
@@ -147,7 +337,21 @@ def main():
         if args.get_count:
             leetcode.get_count(args.get_count)
         if args.get_problem:
-            leetcode.get_next_problem()
+            for k, v in leetcode.get_next_problem():
+                print(v['url'])
+                res = input()
+                match res:
+                    case 'completed':
+                        leetcode.increment_count(k)
+                        return
+                    case 'reset':
+                        leetcode.reset_count(k)
+                        return
+                    case 'exit':
+                        return
+                    case _:
+                        continue
+
         if args.completed:
             leetcode.increment_count(args.completed)
 
